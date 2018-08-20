4c028c0bf8ac05468b64a7328f591791cb77473f
w666x <w666x@users.noreply.github.com>
2018-08-20 22:02:20 +0800

    Update update_client.sql

--- update_client.sql
+++ update_client.sql
@@ -1,1 +1,11 @@
+-- 更改用户密码，以什么格式生成
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password' PASSWORD EXPIRE NEVER; 
+-- 更改用户密码
+ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+--  使操作生效
+FLUSH PRIVILEGES; 
+
+
+-- 127.0.0.1 
+-- guests
+-- guests123
